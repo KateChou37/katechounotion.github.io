8d38bd7965555ca96433b89d45d1854be8675731
KateChou <akak37.tw@gmail.com>
2026-01-08 09:56:08 +0800

    Site updated: 2026-01-08 09:56:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/04/hello-world/"];function toRandomPost(){
+var posts=["2026/01/04/hello-world/","2024/01/08/文章標題/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
