5514a23cfc2d0dd1887deb3c41f0a75630d00255
KateChou <akak37.tw@gmail.com>
2026-01-12 09:41:09 +0800

    Site updated: 2026-01-12 09:41:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2025/08/27/hello-world/","2025/08/27/JS如何宣告變數-筆記/"];function toRandomPost(){
+var posts=["2025/08/27/JS如何宣告變數-筆記/","2025/08/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
